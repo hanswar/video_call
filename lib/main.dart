8fc9d57e80dc5ff5da57eabe13b3e20e06ea85cd
hans <hanswar2@gmail.com>
2024-10-27 22:41:29 +0900

    메인화면 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-
-late List<CameraDescription> _camaras;
+import 'package:video_call/screen/home_screen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  _camaras = await availableCameras();
-  runApp(const CameraApp());
-}
-
-class CameraApp extends StatefulWidget {
-  const CameraApp({super.key});
-
-  @override
-  State<CameraApp> createState() => _CameraAppState();
-}
-
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    initializeCamera();
-  }
-
-  initializeCamera() async {
-    try {
-      controller = CameraController(_camaras[0], ResolutionPreset.max);
-      await controller.initialize();
-
-      setState(() {});
-    } catch (e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            print('User denied camera access.');
-            break;
-          default:
-            print(e.description);
-            break;
-        }
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: CameraPreview(controller),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
